8ba43d57d29297e1c77d96bf8c3db20826b5ac59
inseon <dlsline0611@gmail.com>
2025-04-22 17:21:19 +0900

    project create

---
+++ src/main/java/team10/demo/CoffeebeanApplication.java
@@ -0,0 +1,13 @@
+package team10.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeebeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeebeanApplication.class, args);
+	}
+
+}
