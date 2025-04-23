2a942f4031a2aa434bb776ccc442a90032816e18
mikm7243 <mikm7243@naver.com>
2025-04-23 15:22:45 +0900

    Feat: 회원 가입 및 로그인 로직 추가

---
+++ src/main/java/programmers/coffee/domain/user/dto/request/UserSignUpRequest.java
@@ -0,0 +1,37 @@
+package programmers.coffee.domain.user.dto.request;
+
+import lombok.Builder;
+import lombok.Setter;
+import programmers.coffee.domain.user.domain.Role;
+import programmers.coffee.domain.user.domain.User;
+
+@Setter
+public class UserSignUpRequest {
+
+    private String name;
+    private String email;
+    private String password;
+    private String zipCode;
+
+    @Builder
+    private UserSignUpRequest(String name, String email, String password, String zipCode) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.zipCode = zipCode;
+    }
+
+    public User toEntity(Role role, String password){
+        return User.builder()
+                .name(this.name)
+                .email(this.email)
+                .password(password)
+                .zipCode(this.zipCode)
+                .role(role.toString())
+                .build();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
