3c672f14d87a0c3f55ef3e2ad8a4f4a327044c10
mikm7243 <mikm7243@naver.com>
2025-04-23 09:40:29 +0900

    refactor: 어플리케이션 패키지 이름 변경
    - 의미를 잘 나타내도록 변경

--- src/test/java/programmers/coffee/CoffeeBeanApplicationTests.java
+++ src/test/java/programmers/coffee/CoffeeBeanApplicationTests.java
@@ -1,10 +1,10 @@
-package team10.demo;
+package programmers.coffee;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CoffeebeanApplicationTests {
+class CoffeeBeanApplicationTests {
 
 	@Test
 	void contextLoads() {
