2917fd2514f60359e033329416465606f85ceb0a
dbrkdgus00 <chldlfla@gmail.com>
2025-04-24 14:05:12 +0900

    Refactor: OrderItemResponse 내부에 from 메서드 추가

--- src/main/java/programmers/coffee/order_create/dto/OrderResponseDto.java
+++ src/main/java/programmers/coffee/order_create/dto/OrderResponseDto.java
@@ -3,6 +3,8 @@ package programmers.coffee.order_create.dto;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
+import programmers.coffee.order_create.domain.Order;
+import programmers.coffee.order_create.domain.OrderItem;
 
 @Getter@Setter
 public class OrderResponseDto {
@@ -20,7 +22,32 @@ public class OrderResponseDto {
         private String itemName;
         private int itemCnt;
         private int price;
+
+        public static OrderItemResponse from(OrderItem oi) {
+            OrderItemResponse r = new OrderItemResponse();
+            r.setItemName(oi.getItemName());
+            r.setItemCnt(oi.getOrderCnt());
+            r.setPrice(oi.getPrice());
+            return r;
+        }
     }
+
+    public static OrderResponseDto from(Order order, List<OrderItemResponse> items) {
+        OrderResponseDto dto = new OrderResponseDto();
+        dto.setEmail(order.getEmail());
+        dto.setName(order.getName());
+        dto.setZipCode(order.getZipCode());
+        dto.setAddress(order.getAddress());
+        dto.setTotalPrice(order.getTotalPrice());
+        dto.setItems(items);
+        return dto;
+    }
+
+
+
+
+
+
 
 
 
