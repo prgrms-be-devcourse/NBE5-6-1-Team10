756760d01c0fd4b961fed25f9cba55ccc5581a41
mikm7243 <mikm7243@naver.com>
2025-04-24 12:17:16 +0900

    Feat: 회원 가입 검증 추가 및 회원 가입 성공시 redirect 추가
    - 회원 가입 성공시 PRG 패턴으로 메인 페이지 리다이렉트
    - 회원 가입 폼 요청시 빈 객체 추가

--- src/main/java/programmers/coffee/domain/user/dto/request/UserSignUpRequest.java
+++ src/main/java/programmers/coffee/domain/user/dto/request/UserSignUpRequest.java
@@ -1,17 +1,35 @@
 package programmers.coffee.domain.user.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.Setter;
 import programmers.coffee.domain.user.domain.Role;
 import programmers.coffee.domain.user.domain.User;
 
+@Getter
 @Setter
 public class UserSignUpRequest {
 
+    @NotBlank(message = "이름은 필수 입력값 입니다.")
+    @Pattern(regexp = "^[^\\s]+$", message = "이름은 공백을 포함할 수 없습니다.")
     private String name;
+
+    @NotBlank(message = "이메일은 필수 입력값 입니다.")
+    @Pattern(regexp = "^[^\\s]+$", message = "이메일은 공백을 포함할 수 없습니다.")
     private String email;
+
+    @NotBlank(message = "비밀번호는 필수 입력값 입니다.")
+    @Pattern(regexp = "^[^\\s]+$", message = "비밀번호는 공백을 포함할 수 없습니다.")
     private String password;
+
+    @NotBlank(message = "우편번호는 필수 입력값 입니다.")
+    @Pattern(regexp = "^[^\\s]+$", message = "우편번호는 공백을 포함할 수 없습니다.")
     private String zipCode;
+
+    public UserSignUpRequest() {
+    }
 
     @Builder
     private UserSignUpRequest(String name, String email, String password, String zipCode) {
@@ -21,7 +39,7 @@ public class UserSignUpRequest {
         this.zipCode = zipCode;
     }
 
-    public User toEntity(Role role, String password){
+    public User toEntity(Role role, String password) {
         return User.builder()
                 .name(this.name)
                 .email(this.email)
@@ -31,7 +49,4 @@ public class UserSignUpRequest {
                 .build();
     }
 
-    public String getPassword() {
-        return password;
-    }
 }
