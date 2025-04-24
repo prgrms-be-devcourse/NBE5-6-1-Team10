85e22f7a3019b051e658859342cd449cd9907f1b
dbrkdgus00 <chldlfla@gmail.com>
2025-04-24 23:34:06 +0900

    Refactor: Role 타입 수정

--- src/main/java/programmers/coffee/domain/user/domain/User.java
+++ src/main/java/programmers/coffee/domain/user/domain/User.java
@@ -1,6 +1,8 @@
 package programmers.coffee.domain.user.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -21,6 +23,7 @@ public class User {
     private String email;
     private String password;
     private String zipCode;
+    @Enumerated(EnumType.STRING)
     private Role role;
 
     @Builder
