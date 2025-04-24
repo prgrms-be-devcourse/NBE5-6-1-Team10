e3fc7d73b5f2f3c56da9ec91bafc00512abaf545
Jun <wnsur1234@gmail.com>
2025-04-24 17:50:59 +0900

    Fix : ㅅtest코드  오류 수정

--- src/main/java/programmers/coffee/domain/item/repository/ItemRepository.java
+++ src/main/java/programmers/coffee/domain/item/repository/ItemRepository.java
@@ -1,12 +1,14 @@
 package programmers.coffee.domain.item.repository;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 import programmers.coffee.domain.item.domain.Item;
 
 import java.util.List;
 
 
 @Mapper
+@Repository
 public interface ItemRepository{
 
     void insertItem(Item item);
