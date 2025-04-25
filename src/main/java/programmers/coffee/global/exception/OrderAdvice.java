bb7c186b5d003fcffa17686be7b057972c461242
dbrkdgus00 <chldlfla@gmail.com>
2025-04-26 01:54:16 +0900

    Refactor: 범용성 위해 클래스 이름 변경

--- src/main/java/programmers/coffee/global/exception/OrderAdvice.java
+++ src/main/java/programmers/coffee/global/exception/OrderAdvice.java
@@ -6,19 +6,19 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class OutOfStockAdvice {
+public class OrderAdvice {
 
     @ExceptionHandler(OutOfStockException.class)
     public String handleOutOfStockForOrder(HttpServletRequest request, Model model, OutOfStockException e) {
-        // 요청 URI로 구분해서 order/team 과 일반 order 구분
+
         String uri = request.getRequestURI();
         String message = "재고가 부족합니다. 다른 상품 선택해주세요!";
         model.addAttribute("errorMessage", message);
 
         if (uri.contains("/orders/team")) {
-            return "order/order-team"; // templates/order/order-team.html
+            return "order/order-team"; // templates/order/orderMemberForm.html
         } else {
-            return "order/order"; // templates/order/order.html
+            return "order/order"; // templates/order/orderForm.html
         }
     }
 
