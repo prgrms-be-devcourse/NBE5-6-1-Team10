1037d5d3001a348a41c8a225803f5ab1579d3a42
dbrkdgus00 <chldlfla@gmail.com>
2025-04-26 15:21:50 +0900

    Feat: OutOfStockException 처리하는 controllerAdvice 추가

--- src/main/java/programmers/coffee/global/exception/OrderAdvice.java
+++ src/main/java/programmers/coffee/global/exception/OrderAdvice.java
@@ -1,12 +1,21 @@
 package programmers.coffee.global.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.List;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import programmers.coffee.domain.item.domain.Item;
+import programmers.coffee.domain.item.repository.ItemRepository;
 
 @ControllerAdvice
 public class OrderAdvice {
+
+    private final ItemRepository itemRepository;
+
+    public OrderAdvice(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
 
     @ExceptionHandler(OutOfStockException.class)
     public String handleOutOfStockForOrder(HttpServletRequest request, Model model, OutOfStockException e) {
@@ -15,10 +24,13 @@ public class OrderAdvice {
         String message = "재고가 부족합니다. 다른 상품 선택해주세요!";
         model.addAttribute("errorMessage", message);
 
-        if (uri.contains("/orders/team")) {
-            return "order/order-team"; // templates/order/orderMemberForm.html
+        List<Item> itemList = itemRepository.findAll();
+        model.addAttribute("items", itemList);
+
+        if (uri.contains("/orders/member")) {
+            return "order/orderMemberForm"; // templates/order/orderMemberForm.html
         } else {
-            return "order/order"; // templates/order/orderForm.html
+            return "order/orderForm"; // templates/order/orderForm.html
         }
     }
 
